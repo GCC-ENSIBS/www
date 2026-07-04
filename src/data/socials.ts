027cec9aac776ed82bffc760af0d737c63b77fb6
AntwortEinesLebens <AntwortEinesLebens@proton.me>
2026-07-05 01:44:37 +0200

    feat: ✨ Add social data

---
+++ src/data/socials.ts
@@ -0,0 +1,8 @@
+export const socials = {
+	email: "contact@gcc-ensibs.fr",
+	discord: "https://discord.gg/gcc-ensibs",
+	github: "https://github.com/gcc-ensibs",
+	linkedin: "https://www.linkedin.com/company/gcc-ensibs",
+	x: "https://x.com/gcc_ensibs",
+	youtube: "https://www.youtube.com/@gcc-ensibs",
+};
